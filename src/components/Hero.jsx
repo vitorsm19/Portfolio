b681b65d38cc26dae2996f66489d87a294d3e2d7
vitorsm19 <vitormesquita190902@gmail.com>
2023-02-03 00:29:39 +0100

    Hero section, structure and styles.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "../css/Hero.css";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-container">
         <div className="hero-title">
-        <span className="name">
-          Hey, I'm Vitor Mesquita
-          </span>
+          <span className="name">Hey, I'm Vitor Mesquita</span>
           <h1 className="front">FRONTEND</h1>
           <h1 className="dev">DEVELOPER</h1>
           <span className="location">
@@ -19,12 +18,21 @@ const Hero = () => {
         <img src="/src/assets/about-pic.png" alt="Picture of Vitor Mesquita" />
       </div>
       <div className="hero-text">
-      <p>
-      Frase de efeito com typing effect...
-      Frase de efeito com typing effect...
-      Frase de efeito com typing ef...|
-      </p>
-        </div>
+        <p>Creative dev with a passion for crafting beautiful and </p>
+        <Typewriter
+          words={[
+            "responsive web applications",
+            "fast web applications",
+            "accessible web applications",
+          ]}
+          loop={1}
+          cursor
+          cursorColor="#424dff"
+          typeSpeed={80}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </div>
     </section>
   );
 };
