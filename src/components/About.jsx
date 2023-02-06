6a469b728ac82a0c8ba168b6b66274f7bf4d500a
vitorsm19 <vitormesquita190902@gmail.com>
2023-02-06 04:37:00 +0100

    Refactoring structure and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import "../css/Hero.css";
+import "../css/About.css";
 
 const About = () => {
   return (
     <section className="about">
-      <div className="about-tile">
-
+      <div className="about-container">
+        <div className="about-title">
+          <h1>ABOUT</h1>
+          <h1>ME</h1>
+        </div>
+        <div className="about-text">
+          <span>
+            I'm a detail-oriented developer, always bringing a unique
+            perspective to the team. <br />
+            Self-starter and a fast learner, looking for a position where I can
+            continue to grow as a developer. <br />
+            <br />
+            Check out my <span className="font-blue">skills</span> and
+            <span className="font-blue"> projects</span> below!
+          </span>
+        </div>
       </div>
-      <div className="about-text"></div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
