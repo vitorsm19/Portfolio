c064d34a0ab8ec1fdac199b837e5c4bcc7bd2540
vitorsm19 <vitormesquita190902@gmail.com>
2023-02-06 04:35:59 +0100

    Skills section, structure and styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "../css/Skills.css";
+import {
+  Html,
+  Css,
+  Javascript,
+  Reactjs,
+  Angular,
+  Node,
+  Tailwind,
+  Bootstrap,
+  Csharp,
+  Dotnet,
+  Jquery,
+} from "../components/SkillsLogo.jsx";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="skills">
+      <div className="skills-container">
+        <div className="skills-text">
+          <p className="first">Focused on</p>
+          <div className="logos">
+            <Html />
+            <Css />
+            <Javascript />
+            <Reactjs />
+          </div>
+          <p className="second">but also knowledgeable in</p>
+          <div className="other-logos">
+            <Angular />
+            <Node />
+            <Tailwind />
+            <Bootstrap />
+            <Csharp />
+            <Dotnet />
+            <Jquery />
+          </div>
+        </div>
+        <div className="skills-title">
+          <h1>TECH/</h1>
+          <h1>STACK</h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
